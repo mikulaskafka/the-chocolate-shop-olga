0a04bbcd517e58c758e6628749a6770719c53247
mikulaskafka <149764581+mikulaskafka@users.noreply.github.com>
2024-04-08 08:35:09 +0200

    Update cart.js
    add_to_cart event pushed to dataLayer

--- cart.js
+++ cart.js
@@ -1,19 +1,36 @@
-const addToCartButtons = document.querySelectorAll(
-  ".add-to-cart-btn"
-);
+document.addEventListener('DOMContentLoaded', () => {
+  const addToCartButtons = document.querySelectorAll(".add-to-cart-btn");
 
-addToCartButtons.forEach((button) => {
-  button.addEventListener("click", (event) => {
-    const chocolateCard = event.target.parentElement;
-    const chocolateName =
-      chocolateCard.querySelector("h3").textContent;
+  addToCartButtons.forEach((button, index) => {
+    button.addEventListener("click", () => {
+      const chocolateCard =
+        button.closest('.chocolate-card');
+      const chocolateName =
+        chocolateCard.querySelector("h3").textContent;
+      const chocolatePrice =
+        chocolateCard.querySelector("p").textContent.replace(/[^0-9.]/g, '');
+      const chocolateList =
+        window.location.pathname.includes("/products.html") ? "Explore our chocolates" : "Featured Chocolates";
 
-window.dataLayer = window.dataLayer || [];
-    
-window.dataLayer.push({
-  'productName': chocolateName
-});
-    
-    alert(`Added ${chocolateName} to the cart`);
+      window.dataLayer = window.dataLayer || [];
+
+      dataLayer.push({
+        event: "add_to_cart",
+        currency: "USD",
+        value: parseFloat(chocolatePrice),
+        ecommerce: {
+          items: [{
+            item_name: chocolateName,
+            item_brand: "The Chocolate Shop",
+            item_category: "Chocolates",
+            item_list_name: chocolateList,
+            index: index + 1
+          }]
+        },
+      });
+
+      alert(`Added ${chocolateName} to the cart`);
+      
+    });
   });
 });
